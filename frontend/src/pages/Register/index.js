1c2ba7ef172a18d2ec1de2262b3603725c1a8411
frontendDieferson <diefersonfrontenddeveloper@gmail.com>
2021-08-14 16:33:01 -0400

    APlicaçao web Frontend - Telas de Login e Registro finalizadas

---
+++ frontend/src/pages/Register/index.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import {FiArrowLeft} from 'react-icons/fi'
+
+import logoImg from '../../assets/logo.svg';
+import "./styles.css"
+
+export default function Register() {
+    return(
+        <div className="register-container">
+            <div className="content">
+                <section>
+                    <img src={logoImg} alt="Seja o Herói" />
+                    <h1>Cadastro</h1>
+                    <p>
+                    Faça seu cadastro, entre na plataforma e ajude pessoas a encontrar
+                    os casos da sua ONG
+                    </p>
+
+                    <Link to="/" className="back-link">
+                        <FiArrowLeft size={16} color="#2F80ED" />
+                        Já tenho Cadastro
+                    </Link>
+                </section>
+
+                <form>
+                    <input 
+                    placeholder="Nome da ONG"
+
+                    />
+                <input
+                type="email"
+                placeholder="E-mail"
+                 />
+                 <input 
+                 placeholder="Whatsapp" />
+                 <div className="input-group">
+                     <input
+                     placeholder="Cidade" />
+                
+                <input 
+                placeholder="UF" />
+                
+                 </div>
+                 <button className="button" type="submit">
+                     Cadastrar
+                 </button>
+                </form>
+            </div>
+        </div>
+    )
+}
